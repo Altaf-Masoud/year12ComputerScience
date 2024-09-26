6768a17ffd1c3918ae4d53d14fb511c8cabc8ee1
Altaf-Masoud <19masouda@centralfoundationboys.co.uk>
2024-09-26 13:28:47 +0000

    New change

--- y12CSClasswork/Classwork.py
+++ y12CSClasswork/Classwork.py
@@ -455,9 +455,30 @@ Ensure proper indentation for the while loop.
 Challenge: 
 Calculate and print the factorial of a given number.'''
 
-number = int(input("Enter a number: "))
-num = 1
+# number = int(input("Enter a number: "))
 
-for i in range(number):
-  print(number*num)
-  num+=1
+# while number != 24:
+#     n = number - 1
+#     num = number*(n)
+#     print(num)
+#     number = int(input("Enter a number: "))
+
+# Python program to find the factorial of a number provided by the user.
+
+# change the value for a different result
+num = int(input("Enter a number: "))
+
+# To take input from the user
+#num = int(input("Enter a number: "))
+
+factorial = 1
+
+# check if the number is negative, positive or zero
+if num < 0:
+   print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+   print("The factorial of 0 is 1")
+else:
+   for i in range(1,num + 1):
+       factorial = factorial*i
+   print("The factorial of",num,"is",factorial) 
